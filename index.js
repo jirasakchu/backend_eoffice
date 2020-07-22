2da4f9dae05df04577fb85bcd0dc98f2071d8f92
Group 4 <pimjitnong@gmail.com>
2020-07-22 15:52:34 -0700

    Line Notification

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ app.use('/', common);
 const document = require('./routes/document');
 app.use('/document', document);
 
+const lineNotify = require('./routes/line-notify');
+app.use('/line-notify', lineNotify);
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`Running on http://localhost:${port}`);
